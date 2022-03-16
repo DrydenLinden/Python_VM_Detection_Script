b2826e00f8dee2175abe7d6d30b5de0f0b40be11
Dryden Linden-Bremner <Drydenlinden@hotmail.com>
2022-03-15 21:42:38 -0700

    Remove excess white spaces

--- VMDetection.py
+++ VMDetection.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 import os
 
-
-
-
 def windowsVMDetection():
 	print("Detecting if in a  windows VM")
-
 
 def linuxVMDetection():
 	virtualization = os.system( "systemd-detect-virt")
@@ -16,7 +12,5 @@ def linuxVMDetection():
 	else: 
 		print(virtualization)
 		return 1
-
-
 
 linuxVMDetection()
